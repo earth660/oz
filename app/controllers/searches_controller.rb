9e1f679139a174e6c73b86623fe2b36a5ff8caa9
earth660 <earth660@gmail.com>
2014-02-21 18:28:00 +0900

    検索準備

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,9 @@
 class SearchesController < ApplicationController
-
+ before_action :set_location
 	def search
+	
+	@location = Post.all
+	
 	end
 	
 	def test1
@@ -9,4 +12,17 @@ class SearchesController < ApplicationController
 	def howto_use
 	end
 	
+	
+	
+	
+	private
+
+    def set_location
+      @location = Post.find_by(:user_id => current_user.id)
+    end
+
+    def location_params
+     params.require(:location).permit(:address, :latitude, :longitude)
+    end
+
 end
